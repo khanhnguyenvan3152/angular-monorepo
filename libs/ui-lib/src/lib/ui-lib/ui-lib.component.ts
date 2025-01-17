dbe45f6d0b5d67be20553084df046d50ec2cd209
khanhnguyenvan3152 <luncoi4e@gmail.com>
2025-01-17 22:57:30 +0700

    add cd

---
+++ libs/ui-lib/src/lib/ui-lib/ui-lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui-lib',
+  imports: [CommonModule],
+  templateUrl: './ui-lib.component.html',
+  styleUrl: './ui-lib.component.css',
+})
+export class UiLibComponent {}
